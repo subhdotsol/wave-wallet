02edc106780bc818dcf967c02ee3b14c4ed5cad7
subh <subhajitchaudhury05@gmail.com>
2026-02-14 12:40:33 +0530

    feat : screen1-onboarding finished

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,18 @@
 const { getDefaultConfig } = require("expo/metro-config");
-const { withNativeWind } = require('nativewind/metro');
+const { withNativeWind } = require("nativewind/metro");
 
-const config = getDefaultConfig(__dirname)
+const config = getDefaultConfig(__dirname);
 
-module.exports = withNativeWind(config, { input: './global.css' })
+// Add SVG transformer support
+config.transformer = {
+    ...config.transformer,
+    babelTransformerPath: require.resolve("react-native-svg-transformer/expo"),
+};
+
+config.resolver = {
+    ...config.resolver,
+    assetExts: config.resolver.assetExts.filter((ext) => ext !== "svg"),
+    sourceExts: [...config.resolver.sourceExts, "svg"],
+};
+
+module.exports = withNativeWind(config, { input: "./global.css" });
